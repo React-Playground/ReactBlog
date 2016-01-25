fb586854e6d98348c8ee5ddea54445550b6cb962
darwin <oleg.umarov@gmail.com>
2016-01-25 17:26:30 -0600

    added create route

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+// import {bindActionCreators} from 'redux';
+import {fetchPosts} from '../actions/index';
+import {Link} from 'react-router';
 
-export default  () => {
-  return <div>List of blog posts</div>
-};
+class PostIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add Post
+          </Link>
+        </div>
+        
+
+      List of blog posts</div>
+    );
+  }
+}
+
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({fetchPosts}, dispatch);
+// }
+// you can just pass {fetch} object straigt into the connect function
+
+export default connect(null, {fetchPosts})(PostIndex);
